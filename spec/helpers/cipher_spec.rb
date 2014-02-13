2b11b6af186f914ee79d4a2d0cecb6e86a8242be
Tyson Kunovsky <tyson@gottman.com>
2014-02-12 18:15:34 -0600

    cipher spec

--- spec/helpers/cipher_spec.rb
+++ spec/helpers/cipher_spec.rb
@@ -3,5 +3,6 @@ require 'spec_helper'
 include Cipher
 
 describe Cipher do
-	pending "I don't have time to build this now, but it's next on the feature list for me"
+	pending "I don't have time to build this now, but it's next on the feature list for me" do
+	end
 end
