67cd02ddf7e928a05b59b572a333d626899f9923
Tyson Kunovsky <tyson@gottman.com>
2014-03-12 15:07:33 -0500

    comments

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-# Here we require all of thefiles which are necessary for the specs and the application itself to run
+# Here we require all of the files which are necessary for the specs and the application itself to run
 
 Dir[File.dirname(__FILE__) + '/../app/helpers/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/../app/models/*.rb'].each {|file| require file }
