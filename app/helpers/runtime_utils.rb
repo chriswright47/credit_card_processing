0ace24fed41388b0f39e72bcec2660e5b8471062
Tyson Kunovsky <tyson@gottman.com>
2014-03-12 15:03:54 -0500

    comments

--- app/helpers/runtime_utils.rb
+++ app/helpers/runtime_utils.rb
@@ -9,15 +9,6 @@ module RuntimeUtils
 
 	CARD_TRANSACTION_INFO = [:name, :amount]
 
-	# The driver method for parsing information correctly.
-	# First we split the info on white space.
-	# Then we check to see if the amoung of info is too big or
-	# Too small (an invalid input) and we return if it is.
-	# We then associate the correct keys(our constants above)
-	# with the correct command line info that was passed in
-	# Finally, we either return an error message if the info
-	# Is invalid, or the valid formatted info which will be used
-	# To initialize or charge/credit a card
 
 	def parse(command_line_info)
 		info = command_line_info.split(" ")
@@ -26,14 +17,7 @@ module RuntimeUtils
 		check_validity(formatted_info)
 	end
 
-	# We get rid of the first unnecessary part of the user's prompt - i.e
-	# 'add' or 'credit'/ 'charge' - and capitalize their name.
-	# We then create a new hash to store the formatted information and 
-	# We use the CARD_CREATION_INFO constant to create newly formatted
-	# Card creation if the user prompt started with add.
-	# If the user's prompt started with credit/charge, we instead use the
-	# CARD_TRANSACTION_INFO constant to create the properly formatted 
-	# transaction information.
+
 	def format_info(split_info)
 		parsed_info = shift_and_capitalize(split_info)
 		formatted_hash = Hash.new
@@ -49,11 +33,7 @@ module RuntimeUtils
 		formatted_hash 
 	end
 
-	# The limit/ amount (the last element of the hash) of the credit/charge need to be in
-	# the correct format - i.e. $1000 - otherwise we return an error.
-	# If info being passed in is for creating a new card, we then check to see that the 
-	# card number is an actual number, otherwise we return an error message. Finally, 
-	# if everything is kosher, we return the correctly formatted info
+
 	def check_validity(formatted_info)
 		unless is_limit_valid?(formatted_info.values.last) 
 			return "Limits and charge ammounts must start with a '$' sign and be followed by a valid whole number."
@@ -66,35 +46,29 @@ module RuntimeUtils
 		formatted_info
 	end
 
-	# is the number to_i the same length as the original number?
-	# is the number greater than 19 digits? 
+
 	def invalid_number?(number)
 		number.length != number.to_i.to_s.length || number.length > 19
 	end
 
-	# the first character of the limit needs to be a $, the rest needs to be a valid number,
-	# the number cannot begin with 0 such as 022323.
+
 	def is_limit_valid?(limit)
 		limit[0] == '$' && !invalid_number?(limit[1..-1]) && limit[1].to_i != 0
 	end
 
-	# first we remove the unnecessary first element - i.e. 'add' or 'credit'
-	# Then we capitalize the user's name which will be inportant later
-	# Because we will always check to see if a user with a capitalized 
-	# name exists before creating a new user
+
 	def shift_and_capitalize(parsed_info)
 		parsed_info.shift 
 		parsed_info.first.capitalize! 
 		parsed_info
 	end
 
-	# once we split up the information, if its split length is greater than 4 or less than 3
-	# the user prompt that was passed in is invalid
+
 	def too_big_or_small?(info)
 		info.length > 4 || info.length < 3
 	end
 
-	# for checking to see if the parsed_info is for adding a new credit card
+
 	def is_this_add_card_info?(parsed_info)
 		parsed_info.length == 3
 	end
--- app/models/application_user_interface.rb
+++ app/models/application_user_interface.rb
@@ -8,7 +8,6 @@ class ApplicationUserInterface
 	# this module contains all the helper methods, such as parse, for the run method
 	include RuntimeUtils
 
-	# Attr readers so that we can do Rspec tests/ pass session_information messages
 	attr_reader :credit_card, :session_information
 
 	def initialize(credit_card)
@@ -21,8 +20,7 @@ class ApplicationUserInterface
 	end
 
 	private
-	# this is the main method of this class, its function is to run the application
-	# until a user enter's done
+	
 	def session_driver
 		display_valid_commands
 		command = command_prompt
@@ -46,15 +44,12 @@ class ApplicationUserInterface
 
 
 	def create_new_card(card_info)
-		# this would be an invalid command with too many words
 		return display_error_message(card_info) unless correct_creation_params(card_info)
-		# for now, we are not allowing users of the same name 
-		return user_warning(card_info[:name]) if fetch_a_card(card_info[:name]) ## deal with getting this in the right format
+		return user_warning(card_info[:name]) if fetch_a_card(card_info[:name])
 		self.session_information << self.credit_card.new(card_info)
 	end
 
-	# this method looks in session_information for a card that matches an owner's name
-	# and if no such card is found nil is returned
+
 	def fetch_a_card(card_owner)
 		self.session_information.each do |element|
 			return element if element.name == card_owner
@@ -62,10 +57,6 @@ class ApplicationUserInterface
 		nil
 	end
 
-	# there could be an argument made to refactor credit_a_card and charge_a_card 
-	# into one method, but because they do completely opposite things I have kept 
-	# them separate despite the duplicative look of the code. These methods
-	# call the card's credit and charge methods respectively
 	
 	def credit_a_card(params)
 		return display_error_message(params) unless correct_length(params)
@@ -74,6 +65,7 @@ class ApplicationUserInterface
 		card.credit(params[:amount])
 	end
 
+
 	def charge_a_card(params)
 		return display_error_message(params) unless correct_length(params)
 		card = fetch_a_card(params[:name].capitalize)
@@ -81,37 +73,46 @@ class ApplicationUserInterface
 		card.charge(params[:amount])
 	end
 
+
 	def user_warning(user_name)
 		puts user_already_exists_message(user_name)
 	end
+
 
 	def correct_length(params)
 		params.length == 2
 	end
 
+
 	def correct_creation_params(params)
 		params.length == 3
 	end
+
 
 	def display_session_summary
 		puts session_summary_message(self.session_information)
 	end
 
+
 	def display_error_message(command)
 		puts error_message(command)
 	end
+
 
 	def display_valid_commands
 		puts welcome_message
 	end
 
+
 	def display_invalid_user_message(user_name)
 		puts invalid_user_message(user_name)
 	end
 
+
 	#See my readme for an explination about this method
 	def finished
 	end
+
 
 	def command_prompt
 		print "Enter a command:"
